4501829f799e0facc0f33c5cd17eb859dc956d56
ab-mtz <bmtzs@me.com>
2023-10-30 10:53:05 +0100

    Update 1-Twosum.py

--- 1-Twosum.py
+++ 1-Twosum.py
@@ -1,7 +1,8 @@
 nums = [2,7,11,15]
 target = 9
 for i in range(len(nums)):
-    for j in range(len(nums)-1):
-        if i + j == target:
+    for j in range(len(nums)-1): 
+        if nums[i] + nums[j] == target:
             print(i, j)
+            break
         
