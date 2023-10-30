2fc14fac70cefd2862838275d07bf3df89e76157
ab-mtz <bmtzs@me.com>
2023-10-30 11:11:57 +0100

    Update Test_Twosum.py

--- Test_Twosum.py
+++ Test_Twosum.py
@@ -1,18 +1,18 @@
 from twosum import two_sum
 
 def test_1two_sum():
-    nums = [2,7,11,15]
+    nums = [2, 7, 11, 15]
     target = 9
     assert two_sum(nums, target) == (0, 1)
 
 
 def test_2two_sum():
-    nums = [3,2,4]
+    nums = [3, 2 ,4]
     target = 6
     assert two_sum(nums, target) == (1, 2)
 
 
 def test_3two_sum():
-    nums = [3,3]
+    nums = [3, 3]
     target = 6
     assert two_sum(nums, target) == (0, 1)
