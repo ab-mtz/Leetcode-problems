12a9c17e6d47496d13144b247aa2b4c6c98a92f2
ab-mtz <bmtzs@me.com>
2023-10-30 10:47:58 +0100

    Add 1-Twosum.py

---
+++ 1-Twosum.py
@@ -0,0 +1,6 @@
+ nums = [2,7,11,15]
+        target = 9
+        for i in range(len(nums))
+            for j in range(len(nums-1))
+                if i + j == target:
+                    return i, j
