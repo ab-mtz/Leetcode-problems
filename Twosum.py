bb9b942786699b455c67a42005d698395c061cbe
ab-mtz <bmtzs@me.com>
2023-10-30 11:19:38 +0100

    Update Twosum.py

--- Twosum.py
+++ Twosum.py
@@ -2,10 +2,13 @@ nums = [2,7,11,15]
 target = 9
 
 def two_sum(nums, target):
-    # Iterate i 
+    # Iterate trough list. take firts element to compare to
     for i in range(len(nums)):
-        for j in range(i + 1,len(nums)): 
+        # Iterate a second time the list starting with the element after i
+        for j in range(i + 1, len(nums)): 
+            # check if the sum of values in list with i, j indexes reach the target
             if nums[i] + nums[j] == target:
+                # if so, return the indexes
                 return i, j    
 
 if __name__ == "__main__":
