9300dc8402db8b37ded4ce317416f65ea1784418
ab-mtz <bmtzs@me.com>
2023-10-30 10:57:09 +0100

    Update 1-Twosum.py

--- 1-Twosum.py
+++ 1-Twosum.py
@@ -1,8 +1,10 @@
 nums = [2,7,11,15]
 target = 9
-for i in range(len(nums)):
-    for j in range(len(nums)-1): 
-        if nums[i] + nums[j] == target:
-            print(i, j)
-            break
-        
+
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)-1): 
+            if nums[i] + nums[j] == target:
+                return i, j                
+if __name__ == "__main__":
+    print(*two_sum(nums, target))
